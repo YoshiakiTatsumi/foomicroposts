d12cb9c697de3dc55417f040c0eb0cee20c570c7
YoshiakiTatsumi <octopusgarden0617@gmail.com>
2016-10-16 12:25:21 +0000

    task3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  
+  before_action :correct_user, only: [:edit, :update]
+   
   def show # 追加
    @user = User.find(params[:id])
   end
@@ -36,6 +37,11 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :email, :location, :password, :password_confirmation)
+  end
+  
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to root_path if @user != current_user
   end
   
 end
