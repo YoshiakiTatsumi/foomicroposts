ec0285fc884c93bd15aaf845d0e453516a73361e
YoshiakiTatsumi <octopusgarden0617@gmail.com>
2016-10-16 04:32:10 +0000

    task1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :user_params, only: [:edit, :update]
   
   def show # 追加
    @user = User.find(params[:id])
@@ -18,11 +19,24 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Update Profile"
+      redirect_to @user #ここを修正
+    else
+      render 'edit'
+    end
+  end
+
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
   
 end
