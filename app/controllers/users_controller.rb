721470ea53c7dc5b2d2913c0c6103589ffbdd976
YoshiakiTatsumi <octopusgarden0617@gmail.com>
2016-10-16 07:40:45 +0000

    task2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-  before_action :user_params, only: [:edit, :update]
   
   def show # 追加
    @user = User.find(params[:id])
@@ -36,7 +35,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :location, :password, :password_confirmation)
   end
   
 end
